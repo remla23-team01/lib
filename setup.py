dedfd11479ab891c3c73512d3522662cb8eea935
Leon Kempen <l.m.kempen@outlook.com>
2023-06-01 16:05:20 +0200

    Made versioning easier

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from remla01_lib import version
 
 setup(
     name="remla01_lib",
-    version="1.0.0",
+    version=version,
     description="Library for ML steps for REMLA23",
     url="https://github.com/remla23-team01/lib",
     packages=find_packages(),
