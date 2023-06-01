d51ce0b7fa279bef32c33cbd91f8e449c91ba733
Leon Kempen <l.m.kempen@outlook.com>
2023-06-01 15:05:00 +0200

    Lib refactor wip

--- remla01_lib/__init__.py
+++ remla01_lib/__init__.py
@@ -1,3 +1,5 @@
 from .shared_ml_steps import MLSteps
 
+version = "1.0.0"
 
+mlSteps = MLSteps(version)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="remla01_lib",
+    version="1.0.0",
+    description="Library for ML steps for REMLA23",
+    url="https://github.com/remla23-team01/lib",
+    packages=find_packages(),
+)
