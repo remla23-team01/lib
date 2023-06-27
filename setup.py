f2b03adc8336b3a144bd80c6007426fea3768ec9
Leon Kempen <l.m.kempen@outlook.com>
2023-06-27 10:22:29 +0200

    Fix dependency error

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
-from remla01_lib import version
-
+#from remla01_lib import version
+# TODO make versioning cleaner
 setup(
     name="remla01_lib",
-    version=version,
+    version="1.0.0",
     description="Library for ML steps for REMLA23",
     url="https://github.com/remla23-team01/lib",
     packages=find_packages(),
