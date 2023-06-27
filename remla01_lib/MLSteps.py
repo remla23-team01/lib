b894108ffe2562fc737204b465b43b8b55065dd5
Leon Kempen <l.m.kempen@outlook.com>
2023-06-27 15:07:12 +0200

    Removed not working functions

--- remla01_lib/MLSteps.py
+++ remla01_lib/MLSteps.py
@@ -25,19 +25,6 @@ class MLSteps:
         print("Initialized with version", current_version)
         self.current_version = current_version
 
-    def get_model(self):
-        """
-        Load the model file.
-        """
-        return joblib.load("ml_models/c2_Classifier_Sentiment_Model")
-
-
-    def get_count_vectorizer(self):
-        """
-        Load the CountVectorizer file.
-        """
-        return joblib.load("ml_models/c1_BoW_Sentiment_Model.pkl")
-
 
     def remove_stopwords(self, input: str) -> str:
         """
@@ -58,36 +45,4 @@ class MLSteps:
         ]
         result = " ".join(review)
         #   logger.debug("Stopwords removed.")
-        return result
-
-
-
-    def preprocess_review(self, review: str) -> np.ndarray:
-        """
-        Preprocesses the input review by removing stopwords and transforming it
-        using the provided CountVectorizer.
-
-        Args:
-            review (str): The review to preprocess.
-
-        Returns:
-            np.ndarray: The preprocessed and transformed review.
-        """
-        review = self.remove_stopwords(review)
-        cv = self.get_count_vectorizer()
-        X = cv.transform([review]).toarray()
-        return X
-
-    def classify_review(self, review: str):
-        """
-        Makes a prediction based on the model and the input review.
-
-        Args:
-            review (str): The review to classify.
-
-        Returns:
-            int: The predicted sentiment label.
-        """
-        model = self.get_model()
-        result = model.predict(review)
         return result
