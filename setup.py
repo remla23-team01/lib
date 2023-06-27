924c9709d7dce9813d33c3dde7a9c913424368b5
levijn <levijndejager@gmail.com>
2023-06-27 12:31:36 +0200

    automaticly insert version when realeasing new tag

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
-from remla01_lib import version
+import json
+
+versionfile = json.load("remla01_lib/version.json")
+ver = versionfile['version']
 
 setup(
     name="remla01_lib",
-    version=version,
+    version=ver,
     description="Library for ML steps for REMLA23",
     url="https://github.com/remla23-team01/lib",
     packages=find_packages(),
